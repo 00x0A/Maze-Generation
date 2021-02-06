c8a914166152616558e7adea9c338138bc76c15a
00x0A <ajaykrishnaanandhan@yahoo.com>
2021-02-06 20:35:47 +0530

    Declared the class for Maze.

--- maze.js
+++ maze.js
@@ -1,2 +1,14 @@
 let maze = document.querySelector(".maze");
 let ctx  = maze.getContext("2d");
+
+class Maze
+{
+    constructor( space, rows, columns )
+    {
+        this.space   =   space;
+        this.rows    =   rows;
+        this.columns =   columns;
+        this.grid    =   [];       // Produces Maze.
+        this.stack   =   [];
+    }
+}
